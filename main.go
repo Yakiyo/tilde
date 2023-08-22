657fc637c38fa4125c4870838931b7c189909a53
Yakiyo <yakiyo1206@gmail.com>
2023-08-22 15:55:59 +0000

    feat: panic handler

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 // Fast tldr console client
 package main
 
-import "github.com/Yakiyo/tilde/cmd"
+import (
+	"github.com/Yakiyo/tilde/cmd"
+	"github.com/charmbracelet/log"
+)
 
 func main() {
+	defer panicHandler()
+
 	cmd.Execute()
+}
+
+// handle unexpected panics
+func panicHandler() {
+	if err := recover(); err != nil {
+		log.Error("Unhandled error cause panic. Please consider filing a bug at https://github.com/Yakiyo/tilde")
+		log.Fatal(err)
+	}
 }
