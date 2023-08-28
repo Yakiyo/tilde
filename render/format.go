5cc4123a1a210ea81ba65554b80742f716891e4b
Yakiyo <yakiyo1206@gmail.com>
2023-08-28 11:56:41 +0000

    feat: full fledged formatter

--- render/format.go
+++ render/format.go
@@ -1,32 +1,43 @@
 package render
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/fatih/color"
 )
 
+var variableRegex = regexp.MustCompile(`\{\{(.*)\}\}`)
+var underline = color.New(color.Underline).Sprint
+
 // format markdown
 func format(md string) []string {
 	lines := strings.Split(md, "\n")
 	res := []string{}
-	res = append(res, "")
+	res = append(res, "\n")
 	for _, line := range lines {
 		if strings.HasPrefix(line, "#") {
+			// command name
 			log.Debug("Skipping command name")
 			continue
 		} else if strings.HasPrefix(line, ">") {
 			// command description
-			res[0] += color.BlueString(line) + "\n"
+			res[0] += line[1:] + "\n"
 		} else if strings.HasPrefix(line, "-") {
 			// example description
-			res = append(res, line[1:])
+			res = append(res, color.HiGreenString(line[1:]))
 		} else if strings.HasPrefix(line, "`") {
 			// example
-			line = line[1 : len(line)-1]
-			res = append(res, "      "+line)
+			line = highLightVariable(line[1 : len(line)-1])
+			res = append(res, "      "+color.CyanString(line))
 		}
 	}
+	res = append(res, "")
 	return res
+}
+
+// parse an `example`, remove `{{`, `}}` and underline the variable
+func highLightVariable(line string) string {
+	return variableRegex.ReplaceAllString(line, underline("$1"))
 }
